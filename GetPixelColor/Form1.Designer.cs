dfa6fb1a0c7d2aac31a42bc742970c633754c0df
PDDaniel <daniel@proper-dog.com>
2016-03-30 12:33:26 +0200

    Initial Commit

---
+++ GetPixelColor/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace GetPixelColor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.MouseTimer = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblPixelColor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // MouseTimer
+            // 
+            this.MouseTimer.Interval = 1;
+            this.MouseTimer.Tick += new System.EventHandler(this.OnMouseTimerTick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 63);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Begin";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblPixelColor
+            // 
+            this.lblPixelColor.AutoSize = true;
+            this.lblPixelColor.Location = new System.Drawing.Point(98, 197);
+            this.lblPixelColor.Name = "lblPixelColor";
+            this.lblPixelColor.Size = new System.Drawing.Size(0, 17);
+            this.lblPixelColor.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.lblPixelColor);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer MouseTimer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblPixelColor;
+    }
+}
+
---
+++ GetPixelColor/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetPixelColor
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern bool GetCursorPos(ref Point lpPoint);
+
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+        public static extern int BitBlt(IntPtr hDC, int x, int y, int nWidth, int nHeight, IntPtr hSrcDC, int xSrc, int ySrc, int dwRop);
+
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+       
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public void DoMouseClick(Point location)
+        {
+            //Call the imported function with the cursor's current position
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, uint.Parse(location.X.ToString()), uint.Parse(location.Y.ToString()), 0, 0);
+        }
+
+        Bitmap screenPixel = new Bitmap(1, 1, PixelFormat.Format32bppArgb);
+        public Color GetColorAt(Point location)
+        {
+            using (Graphics gdest = Graphics.FromImage(screenPixel))
+            {
+                using (Graphics gsrc = Graphics.FromHwnd(IntPtr.Zero))
+                {
+                    IntPtr hSrcDC = gsrc.GetHdc();
+                    IntPtr hDC = gdest.GetHdc();
+                    int retval = BitBlt(hDC, 0, 0, 1, 1, hSrcDC, location.X, location.Y, (int)CopyPixelOperation.SourceCopy);
+                    gdest.ReleaseHdc();
+                    gsrc.ReleaseHdc();
+                }
+            }
+
+            return screenPixel.GetPixel(0, 0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!MouseTimer.Enabled)
+            {
+                button1.Text = "Stop";
+                MouseTimer.Start();
+            }
+            else
+            {
+                button1.Text = "Begin";
+                MouseTimer.Stop();
+            }
+        }
+
+        private void OnMouseTimerTick(object sender, EventArgs e)
+        {
+            Point cursor = new Point();
+            GetCursorPos(ref cursor);
+            var c = GetColorAt(cursor);
+            this.BackColor = c;
+
+            if (c.A == 255 && c.R == 75 && c.G == 219 && c.B == 106) // Green background
+            {
+                lblPixelColor.Text = "CLICK";
+                Thread.Sleep(50);
+                DoMouseClick(cursor);
+            }
+        }
+    }
+}
+
